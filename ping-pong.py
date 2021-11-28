b6d59f08ddf6f1e53caecc4734512f32993d4edc
hexyatina <93482167+hexyatina@users.noreply.github.com>
2021-11-28 16:56:02 +0200

    Add files via upload

--- ping-pong.py
+++ ping-pong.py
@@ -8,13 +8,32 @@ window = display.set_mode((w_width,w_height))
 display.set_caption("ping-pong")
 clock = time.Clock()
 
-speed_x = 4
-speed_y = 4
+speed_x = 7
+speed_y = 7
+
+
+racket_height = 50
+
+ball_h_w = 25
 
 def set_difficulty(*args):
+    global speed_x, speed_y, racket_height, ball_h_w
 
+    if args[1] == 3:
+        speed_x = 5
+        speed_y = 5
+        racket_height = 95
+        ball_h_w = 50
+    if args[1] == 2:
+        speed_x = 3
+        speed_y = 3
+        racket_height = 115
+        ball_h_w = 75
     if args[1] == 1:
-        print("Сложна")
+        speed_x = 7
+        speed_y = 7
+        racket_height = 50
+        ball_h_w = 25
 def start_the_game():
     run = True
     game = True
@@ -32,15 +51,11 @@ def start_the_game():
     #Font.init()
     font1 = font.SysFont('ComicSansMC', 50)
     win1 = font1.render(
-        'Игрок 1 победил', True, (255, 215, 0)
+        'Игрок 1 победил', True, (0, 0, 0)
     )
     win2 = font1.render(
-        'Игрок 2 победил', True, (255, 215, 0)
+        'Игрок 2 победил', True, (0, 0, 0)
     )
-    score1 = font1.render(
-        '0', True, (255, 215, 0))
-    score2 = font1.render(
-        '0', True, (255, 215, 0))
     class GameSprite(sprite.Sprite):
         def __init__(self, playerimage, playerx, playery, spr_width, spr_height, playerspeed):
             super().__init__()
@@ -71,6 +86,8 @@ def start_the_game():
             self.rect.y += speed_y
             if self.rect.y > w_height-50 or self.rect.y < 0:
                 speed_y *= -1
+            if sprite.collide_rect(racket, ball) or sprite.collide_rect(racket2, ball):
+                speed_x *= -1
     '''class T_ball(GameSprite):
         def update_ball(self):
             self.rect.x += speed_x
@@ -79,39 +96,36 @@ def start_the_game():
                 or self.rect.y < 0:
                 speed_y *= -1'''
 
-    racket = Racket("racket.png", 630, 100, 95, 25, 10)
-    racket2 = Racket("racket2.png", 20, 100, 95, 25, 10)
-    ball = Racket("tenis_ball.png", 350, 300, 25, 25, 3)
+    racket = Racket("racket.png", 630, 100, racket_height, 25, 10)
+    racket2 = Racket("racket2.png", 20, 100, racket_height, 25, 10)
+    ball = Racket("tenis_ball.png", 350, 300, ball_h_w, ball_h_w, 3)
     while run:
         for e in event.get():
             if e.type == QUIT:
                 run = False
         if not finish:
-            window.fill((155, 0, 255))
-            '''ball.rect.x += speed_x
-            ball.rect.y += speed_y'''
+            window.fill((255, 165, 0))
+            # ball.rect.x += speed_x
+            # ball.rect.y += speed_y
             racket.update_l()
             racket.reset()
             racket2.update_r()
             racket2.reset()
-            ball.update()
+            ball.update_ball()
             ball.reset()
-            window.blit(score1, (320, 0))
-            window.blit(score2, (370,0))
             
-            '''ball.rect.x += speed_x
-            ball.rect.y += speed_y'''
+            
+            #ball.rect.x += speed_x
+            #ball.rect.y += speed_y
 
-            if ball.rect.y > w_height-50 or ball.rect.y < 0:
-                speed_y *= -1
-            if sprite.collide_rect(racket, ball) or sprite.collide_rect(racket2, ball):
-                speed_x *= -1
-        if ball.rect.x < 0:
-            finish = True
-            window.blit(win1, (200,200))
-        if ball.rect.x > 680:
-            finish = True
-            window.blit(win2, (200,200))
+
+
+            if ball.rect.x < 0:
+                finish = True
+                window.blit(win1, (200,200))
+            if ball.rect.x > 680:
+                finish = True
+                window.blit(win2, (200,200))
         display.update()
         clock.tick(60)
 menu = pygame_menu.Menu("Привет-привет", 350, 300, theme = pygame_menu.themes.THEME_SOLARIZED)
